a08fa908ac5487740dea781c94c3ab901bcee95a
wyandrew <andrewsit.wy@gmail.com>
2020-04-15 00:20:33 +0800

    First commit

---
+++ src/robots.js
@@ -0,0 +1,69 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Paul Choi',
+      username: 'Paul.Choi',
+      email: 'paul@robofriends.com'
+    },
+    {
+      id: 2,
+      name: 'Bess Kong',
+      username: 'Bess.Kong',
+      email: 'bess@robofriends.com'
+    },
+    {
+      id: 3,
+      name: 'Jacky Kong',
+      username: 'Jacky.Kong',
+      email: 'jacky@robofriends.com'
+    },
+    {
+      id: 4,
+      name: 'Andrew Sit',
+      username: 'andrewsit',
+      email: 'andrew@robofriends.com'
+    },
+    {
+      id: 5,
+      name: 'Akiko Li',
+      username: 'Akiko.Li',
+      email: 'akiko@robofriends.com'
+    },
+    {
+      id: 6,
+      name: 'Rinky Chung',
+      username: 'Rinky.Chung',
+      email: 'rinky@robofriends.com'
+    },
+    {
+      id: 7,
+      name: 'Jasmine Wong',
+      username: 'Jasmine.Wong',
+      email: 'jasmine@robofriends.com'
+    },
+    {
+      id: 8,
+      name: 'Flora Yeung',
+      username: 'Flora.Yeung',
+      email: 'flora@robofriends.com'
+    },
+    {
+      id: 9,
+      name: 'Coco Leung',
+      username: 'Coco.Leung',
+      email: 'coco@robofriends.com'
+    },
+    {
+      id: 10,
+      name: 'Franky Kong',
+      username: 'Franky.Kong',
+      email: 'franky@robofriends.com'
+    }
+    {
+      id: 11,
+      name: 'Annie Sit',
+      username: 'Annie.Sit',
+      email: 'annie@robofriends.com'
+    }
+  ];
+  
